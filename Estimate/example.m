016d14f72c0491402b7bb47e7b219fdb8b6333ff
Jack Pierce-Brown <jackpiercebrown@gmail.com>
2018-08-31 13:14:13 +0100

    improved documentation

--- Estimate/example.m
+++ Estimate/example.m
@@ -1,5 +1,8 @@
 % example.m is a script providing examples of how to use estimation tools.
 
+%% Example 1 - inference from data
+
+% simulate data
 nx = 1e2;
 lx = 10;
 dx  = lx/nx;
@@ -11,52 +14,43 @@ mu  = 10;
 sigma = 3;
 method = 'eig';
 nSamples = 6;
-
-samples = CMD1D(points,corFun,corLen,dist,mu,sigma,method,nSamples);
-
-%condPoints = [0.5 1.3 1.5 2.8 4 4.2]';
-%condVals = [7.7 7.6 7.2 6.3 7.1 6.9]';
+samples = CMD1D(points,corFun,corLen,dst,mu,sigma,method,nSamples);
 
 % visualise data
 fig1 = figure(1);
 plot(samples)
 
-%% Method of moments
-
-% mu
+% method of moments
 mu = mean(samples(:))
-
-% sigma
 sigma = std(samples(:))
 
-% lag1
+% lag-1 correlation
 corrVal = lag1corr(samples);
 thetaHat = lag1theta(corrVal,dx,corFun)
 
-% full acf (FFT and non-FFT)
-%ACF = autocorr(samplesMat(:,1),nx-1);
+% acf
 ACF2 = corrFFT(samples);
-figure(2)
+thetaHat = fitACF(ACF2,dx,corFun,lx);
+
+fig2 = figure(2);
 plot(ACF2)
 
-thetaHat = fitACF(ACF2,dx,corFun,lx)
-
-% < figure of ACF >
-
-%% Maximum likelihood
-% mu, sigma theta
+% maximum likelihood estimation
 [thetaMLE,muMLE,sigmaMLE] = maxLfun(samples,points,corFun,0,lx);
 
-%% Kriging
-
-condPoints = [1,2,3,4,5,6,7]';
-condVals = [5,6,7,6,4,5,4]';
+%% Example 2 - Kriging
+uncondPoints = linspace(0,8,1e3)';
+condPoints = [0.5 1.3 1.5 2.8 4 4.2]';
+condVals = [7.7 7.6 7.2 6.3 7.1 6.9]';
+mu = 7;
+theta = 1.5;
+sigma = 0.5;
 
 corFun = 'sexp';
 
 [krige,CIupper,CIlower] = krigeIt(condPoints,condVals,uncondPoints,corFun,mu,sigma,theta);
 
-figure(3)
+fig3 = figure(3);
 hold on
 X = [uncondPoints; flipud(uncondPoints)];
 Y = [CIlower; flipud(CIupper)];
@@ -65,5 +59,5 @@ set(h,'EdgeColor','none')
 plot(uncondPoints,krige,'color',[1 0 0],'LineWidth',2)
 scatter(condPoints,condVals,'MarkerEdgeColor',[1 0 0],'LineWidth',2,'Marker','o','SizeData',50,'LineWidth',2,'MarkerFaceColor',[1,1,1]);
 hold off
-grid on
 box on
+set(fig3,'pos',[100,100,1200,500])
